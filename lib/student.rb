998e59b432c4c2b6902e13e490c8a903900ae59c
Matthew Peoples <matthew.peoples@yale.edu>
2019-06-09 20:44:19 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
   #  with DB[:conn]
   attr_accessor :name, :grade, :id
 
-  def initialize(name, grade, id=nil)
+  def initialize(id=nil, name, grade)
     @name = name
     @grade = grade
     @id = id
@@ -37,13 +37,14 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
-    student = self.new(row[1], row[2], row[0])
+    stud_id = row[0]
+    stud_name = row[1]
+    stud_grade = row[2]
+    student = self.new(stud_id, stud_name, stud_grade)
     student
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
     sql = <<-SQL
       SELECT * FROM students
       WHERE name = ?
